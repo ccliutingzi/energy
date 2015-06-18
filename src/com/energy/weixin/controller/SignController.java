53172c9de1725d2391d1abe567d03a333613caf6
ccliu2015 <719923011@qq.com>
2015-06-18 15:53:22 +0800

    修改文件

--- src/com/energy/weixin/controller/SignController.java
+++ src/com/energy/weixin/controller/SignController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.energy.weixin.entity.Sign;
 import com.energy.weixin.mapper.SignMapper;
+import com.energy.weixin.utils.CommonUtil;
 
 /** 
  * @ClassName: SignController 
@@ -23,10 +25,21 @@ public class SignController {
 	@Autowired
 	private SignMapper signMapper;
 
+	/**
+	 * 签到处，显示今天所有的签到信息
+	 * @return
+	 */
+	@RequestMapping(value="signsOfToday", method = RequestMethod.GET)
+	public ModelAndView signsOfToday() {
+		ModelAndView view = new ModelAndView("sign_add");
+		view.addObject("result", "OK");
+		return view;
+	}
+	
 	@RequestMapping(value="addSign", method = RequestMethod.GET)
 	public String addSign(ModelMap model) {
 		try{
-			signMapper.addSign(new Sign("1", "123", "刘闯闯", null, null, null, null));
+			signMapper.addSign(new Sign(CommonUtil.GeneGUID(), "123456", null, null, null, null, null));
 			model.addAttribute("result", "OK");
 		} catch (Exception e){
 			model.addAttribute("result", "ERROR");
---
+++ src/com/energy/weixin/utils/CommonUtil.java
@@ -0,0 +1,29 @@
+package com.energy.weixin.utils;
+
+import java.util.UUID;
+
+/** 
+ * @ClassName: CommonUtil 
+ * @Description: 工具类
+ * @author CCLIU 
+ * @date 2015-6-18 下午3:35:32 
+ * v1.0
+ */
+public class CommonUtil {
+	
+	/**
+	 * 获取32位GUID
+	 * @return
+	 */
+	public static String GeneGUID(){
+		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print(GeneGUID().length());
+	}
+
+}
