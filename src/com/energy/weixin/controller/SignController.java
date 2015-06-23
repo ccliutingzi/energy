570f4da59d81b09f72517ba323d5b5ba7ea29126
ccliu2015 <719923011@qq.com>
2015-06-23 11:02:07 +0800

    设计接口

--- src/com/energy/weixin/controller/SignController.java
+++ src/com/energy/weixin/controller/SignController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.energy.weixin.entity.Sign;
@@ -29,22 +30,81 @@ public class SignController {
 	 * 签到处，显示今天所有的签到信息
 	 * @return
 	 */
-	@RequestMapping(value="signsOfToday", method = RequestMethod.GET)
+	@RequestMapping(value="signsToday", method = RequestMethod.GET)
 	public ModelAndView signsOfToday() {
-		ModelAndView view = new ModelAndView("sign_add");
+		ModelAndView view = new ModelAndView("sign_today");
+		// TODO 取出今天的所有签到信息
 		view.addObject("result", "OK");
 		return view;
 	}
 	
 	/**
-	 * 添加一个签到信息
+	 * 签到详情
+	 * @return
+	 */
+	@RequestMapping(value="signDetail", method = RequestMethod.GET)
+	public ModelAndView signDetail() {
+		ModelAndView view = new ModelAndView("sign_detail");
+		// TODO 取出一个签到的详情
+		view.addObject("result", "OK");
+		return view;
+	}
+	
+	/**
+	 * 进行签到
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="signIn", method = RequestMethod.GET)
+	public String signIn() {
+		// TODO 新增签到信息
+		return "OK";
+	}
+	
+	/**
+	 * 进行签退
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="signOut", method = RequestMethod.GET)
+	public String signOut() {
+		// TODO 新增签退信息
+		return "OK";
+	}
+	
+	/**
+	 * 添加/修改备注
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="editRemark", method = RequestMethod.GET)
+	public String editRemark() {
+		// TODO 编辑签到的备注
+		return "OK";
+	}
+	
+	/**
+	 * 发起签到页面
 	 * @param model
 	 * @return
 	 */
 	@RequestMapping(value="addSign", method = RequestMethod.GET)
 	public String addSign(ModelMap model) {
+		return "sign_add";
+	}
+	
+	/**
+	 * 添加签到信息
+	 * @param model
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="doAdd", method = RequestMethod.GET)
+	public String doAdd(ModelMap model) {
 		try{
+			// TODO 插入数据
 			signMapper.addSign(new Sign(CommonUtil.GeneGUID(), "123456", null, null, null, null, null));
+			// TODO 向相关的人员发送签到消息
 			model.addAttribute("result", "OK");
 		} catch (Exception e){
 			model.addAttribute("result", "ERROR");
@@ -53,11 +113,12 @@ public class SignController {
 	}
 
 	/**
-	 * 查询当月统计
+	 * 根据月份查询当月统计
 	 * @return
 	 */
-	public ModelAndView queryCurrentMonth(){
+	public ModelAndView queryByMonth(){
 		ModelAndView view = new ModelAndView();
+		// TODO 取出当月统计数据
 		view.addObject("result", "OK");
 		return view;
 	}
