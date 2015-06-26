f3d9e68977c1cb750e63191d0cc46ac7350f3704
ccliu2015 <719923011@qq.com>
2015-06-26 14:54:38 +0800

    增加微信校验

--- src/com/energy/weixin/controller/CommonController.java
+++ src/com/energy/weixin/controller/CommonController.java
@@ -1,9 +1,17 @@
 package com.energy.weixin.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.energy.weixin.constant.Constants;
+import com.energy.weixin.utils.ConfigUtil;
+import com.energy.weixin.utils.wxutil.WXBizMsgCrypt;
 
 /** 
  * @ClassName: CommonController 
@@ -14,11 +22,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 @RequestMapping(value = "/common")
-public class CommonController {
+public class CommonController extends AbstWebController{
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String index(ModelMap model) {
-		return "index";
+	/**
+	 * 企业号应用验证
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="weixin", method = RequestMethod.GET)
+	public void weixin(HttpServletRequest request, HttpServletResponse response) {
+		// 请求参数
+		Map<String, Object> params = getRequestParams(request);
+
+		String sCorpID = ConfigUtil.get(Constants.WEIXIN_APP_PATH, "corpId");
+		String sToken = ConfigUtil.get(Constants.WEIXIN_APP_PATH, "token");
+		String sEncodingAESKey = ConfigUtil.get(Constants.WEIXIN_APP_PATH, "encodingAESKey");
+		String sEchoStr = "init"; //需要返回的明文
+		try {
+			WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(sToken, sEncodingAESKey, sCorpID);
+			String sVerifyMsgSig = params.get("msg_signature").toString();
+			String sVerifyTimeStamp = params.get("timestamp").toString();
+			String sVerifyNonce = params.get("nonce").toString();
+			String sVerifyEchoStr = params.get("echostr").toString();
+			
+			sEchoStr = wxcpt.VerifyURL(sVerifyMsgSig, sVerifyTimeStamp,
+					sVerifyNonce, sVerifyEchoStr);
+			System.out.println("verifyurl echostr: " + sEchoStr);
+			response.getOutputStream().write(sEchoStr.getBytes());
+			response.getOutputStream().flush();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 	}
 	
 }
